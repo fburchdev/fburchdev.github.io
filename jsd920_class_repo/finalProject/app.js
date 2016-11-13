71fd4fc94966554772d4bce7acf78acb62988fe8
Frances Burch <fburchdev@gmail.com>
2016-11-13 00:40:32 -0500

    Demo html page to display data from greenSpaces.json

--- jsd920_class_repo/finalProject/app.js
+++ jsd920_class_repo/finalProject/app.js
@@ -5,11 +5,22 @@
 
 $(document).ready(function(document){
 
+    function displayGreenSpaces(data) {
+        console.log("Hello from function displayGreenSpaces");
+        console.log(data);
+        var ul = $('#greenSpaces');
+        data.forEach(function(item) {
+            var li = $('<li>');
+            li.html(item.name);
+            ul.append(li);
+        });
+    };
+
     $.ajax({
         type: "GET",
         url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Brooklyn_Botanic_Garden&callback=?",
         contentType: "application/json; charset=utf-8",
-        async: false,
+        async: true,
         dataType: "json",
         success: function (data, textStatus, jqXHR) {
 
@@ -21,5 +32,26 @@ $(document).ready(function(document){
         error: function (errorMessage) {
             console.log("An Error Occurred while retrieving Wikipedia Article.");
         }
-        });
     });
+
+    $.ajax({
+        type: "GET",
+        url: "data/greenSpaces.json",
+        contentType: "application/json; charset=utf-8",
+        async: true,
+        dataType: "json",
+        success: function (data, textStatus, jqXHR)  {
+            console.log("Retrieving green spaces json");
+            console.log(data);
+            console.log("Calling displayGreenSpaces function");
+            displayGreenSpaces(data);
+
+
+        },
+        error: function (errorMessage) {
+            console.log("An Error Occurred while retrieving green Spaces json");
+        }
+
+    });
+
+});//end document ready function
